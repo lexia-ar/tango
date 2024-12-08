352ebcbf4a02e3b41f0e0f7219875ee13b2a23bf
Tatako Felici <francis.felici@gmail.com>
2024-12-08 19:28:54 -0300

    Create tangoQuant_bnb_4b.py

---
+++ tangoQuant_bnb_4b.py
@@ -0,0 +1,15 @@
+  GNU nano 7.2                                    quant_tango70b_bnb_4b.py                                              import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+bnb_config = BitsAndBytesConfig(load_in_4bit=True,bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_c>
+model_id = 'Model-SafeTensors/Llama-3.1-Tango-70b'
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model_4bit = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map="auto")
+
+model_4bit.save_pretrained("tango-70b_bnb-4bit")
+tokenizer.save_pretrained("tango-70b_bnb-4bit")
+
+model_4bit.push_to_hub("sandbox-ai/Llama-3.1-Tango-70b-bnb_4b", token = "...")
+
+tokenizer.push_to_hub("sandbox-ai/Llama-3.1-Tango-70b-bnb_4b", token = "...")
